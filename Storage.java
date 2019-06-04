55a1629a8381e957e6aebb6c5f3887f4f105c48e
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-06-04 23:06:59 +0300

    Update Storage.java

--- Storage.java
+++ Storage.java
@@ -1,1 +1,26 @@
+package com.company;
 
+public class Storage<T, V> {
+
+    private T key;
+    private V value;
+
+    public void put(T key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public V get(T key) {
+        if (this.key == key){
+            return value;
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Storage: " +
+                "key = " + key +
+                ", value = " + value;
+    }
+}
