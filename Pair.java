e900be0017411271fcf081f2687917f760f32ab4
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-06-04 23:08:44 +0300

    Update Pair.java

--- Pair.java
+++ Pair.java
@@ -1,1 +1,81 @@
+package com.company;
 
+import java.util.Objects;
+
+public class Pair<T, V> {
+
+    private T first;
+    private V second;
+
+    private Pair(T first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public static <T, V> Pair<T, V> of(T first, V second) {
+        return new Pair<>(first, second);
+    }
+
+    public V getSecond() {
+        return second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(first, pair.first) &&
+                Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+}
+
+package com.company;
+
+import java.util.Objects;
+
+public class Pair<T, V> {
+
+    private T first;
+    private V second;
+
+    private Pair(T first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public static <T, V> Pair<T, V> of(T first, V second) {
+        return new Pair<>(first, second);
+    }
+
+    public V getSecond() {
+        return second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(first, pair.first) &&
+                Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+}
