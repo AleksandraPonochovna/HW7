5017c1a469511793bd403d258c59e6b0d87a717e
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-06-05 15:20:54 +0300

    Update Pair.java

--- Pair.java
+++ Pair.java
@@ -35,47 +35,10 @@ public class Pair<T, V> {
 
     @Override
     public int hashCode() {
-        return Objects.hash(first, second);
-    }
-}
-
-package com.company;
-
-import java.util.Objects;
-
-public class Pair<T, V> {
-
-    private T first;
-    private V second;
-
-    private Pair(T first, V second) {
-        this.first = first;
-        this.second = second;
-    }
-
-    public static <T, V> Pair<T, V> of(T first, V second) {
-        return new Pair<>(first, second);
-    }
-
-    public V getSecond() {
-        return second;
-    }
-
-    public T getFirst() {
-        return first;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Pair<?, ?> pair = (Pair<?, ?>) o;
-        return Objects.equals(first, pair.first) &&
-                Objects.equals(second, pair.second);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(first, second);
+        int hashCode = 0;
+        int someSimpleNumber = 31;
+        hashCode += first == null ? null : first.hashCode() * someSimpleNumber;
+        hashCode += second == null ? null : second.hashCode() * someSimpleNumber;
+        return hashCode;
     }
 }
